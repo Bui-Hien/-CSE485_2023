a7f38302253bf3c3ac3a79b0e5ffce4e2c3cb869
HoangQuocHuy <hoangquochuy270103@gmail.com>
2023-11-30 11:21:47 +0700

    AddCategory,DeleteCategory,UpdateCategory,Update Sql

--- admin/Connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $conn = new PDO(dsn: "mysql:host=localhost;dbname=booksmanagementsystem", username: "root", password: "");
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-?>
--- admin/category.php
+++ admin/category.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,9 +10,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
 </head>
+
 <body>
     <?php
-    include 'Connection.php';
+    try {
+        $conn = new PDO(dsn: "mysql:host=localhost;dbname=btth01_cse485_ex.sql", username: "root", password: "");
+        $SQL = "SELECT ma_tloai,ten_tloai FROM theloai";
+        $stmt = $conn->prepare($SQL);
+        $stmt->execute();
+        $ListCategory = $stmt->fetchAll();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
     ?>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
@@ -20,26 +30,26 @@
                     <a class="navbar-brand" href="#">Administration</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Trang ngoài</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="author.php">Tác giả</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
-                    </li>
-                </ul>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Trang ngoài</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="author.php">Tác giả</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="article.php">Bài viết</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -53,34 +63,38 @@
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
+                            <th scope="col">Mã Thể Loại</th>
                             <th scope="col">Tên thể loại</th>
                             <th>Sửa</th>
                             <th>Xóa</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Nhạc trữ tình</td>
-                            <td>
-                                <a href="edit_category.php?id=1"><i class="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td>
-                                <a href=""><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Nhạc cách mạng</td>
-                            <td>
-                                <a href="edit_category.php?id=2"><i class="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td>
-                                <a href=""><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
-                       
+                        <?php foreach ($ListCategory as $list) : ?>
+                            <tr>
+                                <th scope="row"><?= $list['ma_tloai']; ?></th>
+                                <td><?= $list['ten_tloai']; ?></td>
+                                <td>
+                                    <input type="hidden" class="form-control" name="update">
+                                    <a href='edit_category.php?id=<?= $list['ma_tloai'] ?>'><i class="fa-solid fa-pen-to-square"></i></a>
+                                    <script>
+                                        function myDelete() {
+                                            var result = confirm("Bạn có muốn xóa không?");
+                                            console.log('1234');
+                                            if (!result) {
+                                                const element = document.getElementById("delete");
+                                                element.href = "";
+                                            }
+                                        }
+                                    </script>
+                                </td>
+                                <td>
+                                    <input type="hidden" class="form-control" name="delete">
+                                    <a href='process_add_category.php?id=<?= $list['ma_tloai']?>' onclick="myDelete()" id="delete"><i class="fa-solid fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+
                     </tbody>
                 </table>
             </div>
@@ -91,4 +105,5 @@
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
 </html>
